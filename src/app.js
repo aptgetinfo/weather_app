6f941872645d0be67f49f0a5f87f2a7e890dedff
Deepak Kumar <deepak.2002.02.16@gmail.com>
2021-06-06 07:46:38 +0530

    Port error resolved

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const forecast=require('./utils/forrecast')
 
 
 const app=express()
-const port=process.env.port || 3000
+const port=process.env.PORT || 3000
 const publicdir=path.join(__dirname,'../public')
 const viewspath=path.join(__dirname,'../tempelates/views')
 const partialpath=path.join(__dirname,'../tempelates/partials')
